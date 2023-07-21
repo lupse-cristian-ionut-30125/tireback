76cfed98792885895959a33961285a36119ddc11
lupse-cristian-ionut-30125 <cristi@freshclick.ro>
2023-07-21 13:58:02 +0300

    fourth

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection('mysql://livingtech_gabriel:parola2001@livingtech.ro/livingtech_ExtremeDB');
+const mysql = require("mysql");
+const connection = mysql.createConnection(
+  "mysql://extremeparts_gabriel:parola2001@extreme-parts.com/extremeparts_ExtremeDB"
+);
 
-connection.connect((err)=>{
-if(err){
-    console.error("an error occured: "+err);
+connection.connect((err) => {
+  if (err) {
+    console.error("an error occured: " + err);
     return;
-}
-console.log("Mysql db connected succesfully")
-
+  }
+  console.log("Mysql db connected succesfully");
 });
 
-module.exports={connection};
+module.exports = { connection };
